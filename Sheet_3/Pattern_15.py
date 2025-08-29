2f4b0d3959501c299445481fcc3d8549884e1690
KRISHNA KUMAR <kartikkrishnakumar18@gmail.com>
2025-08-29 08:18:11 +0530

    Update Pattern_15.py

--- Sheet_3/Pattern_15.py
+++ Sheet_3/Pattern_15.py
@@ -3,8 +3,9 @@ for i in range(0,N+1):
    for j in range(i):
        print("*",end=" ")
    print()
-for i in range(N+1,0,-1):
-    for j in range(i):
+for i in range(N,0,-1):
+    for j in range(i-1):
        print("*",end=" ")
     print()
     
+
